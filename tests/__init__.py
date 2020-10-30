c1230acf99abf0eac986f7d4bad206571b3ece65
Raimon Esteve <raimonesteve@gmail.com>
2020-10-30 13:27:47 +0100

    FIX tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
-# This file is part whoosh module for Tryton.
+# This file is part whooshsearch module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from .test_whoosh import suite
+from .test_whooshsearch import suite
 
 __all__ = ['suite']
