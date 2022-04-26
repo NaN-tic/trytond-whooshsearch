2a6e6e8b98266e1f423bf2a8bf8fa1034969cd67
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 12:45:05 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is part whooshsearch module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-from .test_whooshsearch import suite
-
-__all__ = ['suite']
